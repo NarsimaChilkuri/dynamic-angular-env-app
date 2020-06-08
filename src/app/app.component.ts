7560aca90eecc0c3fc014a4d30ff7e6866d315f3
NarsimaChilkuri <narsimachilkuri235@gmail.com>
2020-06-08 14:08:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppConfigService } from './app-config.service';
+import { environment } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dynamic-env-angular-app';
+  version = '1.0.0';
+  stack = 'dev';
+
+  constructor(private configService: AppConfigService) {
+
+  }
+
+  ngOnInit() {
+    this.title = this.configService.getConfig().name;
+    this.version = this.configService.getConfig().version;
+    this.stack = environment.stack;
+  }
 }
